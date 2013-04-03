4b265342f7ac470b486ef4d571a7b56e67ee18f4
dkkline <jeppe@dapj.dk>
2013-04-03 18:44:22 +0200

    Rewrote examples/Tutorial.py chmod -x TODO

--- examples/Tutorial.py
+++ examples/Tutorial.py
@@ -1,18 +1,6 @@
-import os
-import sys
-sys.path.append(os.path.join("..", "..", ".."))
-# Please teach me the proper way!
-# Forget all of the above, It's horrible code
+from McClient import SimpleClient
+from McClient.Utils import fix_message
 
-from McLib.networking import Connection
-# Contains ServerConnection and SessionConnection
-
-from McLib.networking import PacketListenerManager
-from McLib.networking import PacketSenderManager
-# Our Protocol Handlers
-
-from McLib import EventManager
-# Our default EventManager
 from getpass import getpass
 # Function in standard python to get passwords the unixy way
 
@@ -23,26 +11,24 @@ password = getpass()
 host = raw_input("Server (IP): ")
 port = int(raw_input("Server (Port): "))  # Remember to turn to integer
 
-session = Connection.SessionConnection(username, password)
-session.connect()
-# Create a connection to session.minecraft.net
 
-sender = PacketSenderManager.Protocol_49()
-listener = PacketListenerManager.Protocol_49()
-# Create our Protocol Handlers, protocol version 49 (1.4.4 and 1.4.5)
-
-
-class MyEventManager(EventManager.Protocol_49):
-    def event03(self, direct, message):
+class Events(object):
+    @staticmethod
+    def recv_chat_message(message):
+        message = fix_message(message)  # Fix escapes, colors, etc
         print "CHAT: %s" % message
 
-    def eventFF(self, direct, reason):
+    @staticmethod
+    def recv_client_disconnect(reason):
         print "We got kicked: %s" % reason
 
-event_manager = MyEventManager()
-# Custom EventManager, override chat event.
-
-con = Connection.ServerConnection(session, host, port, sender, listener,
-                                  event_manager=event_manager)
-con.connect()
+client = SimpleClient()
+client.eventmanager.apply(Events())
+client.connect(host, port, username, password)
 # Server connection.
+
+raw_input()  # Since connection runs in a seperate thread, we can't just CTRL-C
+client.disconnect()  # We can call the wrapper function, disconnect, to nicely
+# disconnect, it also has a force KWarg
+
+# For a better chat client, see: https://github.com/dkkline/pyCraft
