6b5bff905f3170ee6751f04d9cefa18072c35b2f
dkkline <jeppe@dapj.dk>
2013-07-05 23:49:50 +0200

    Updated test.py, now supports non-interactive mod

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from McClient import SimpleClient
 from getpass import getpass
 import time
+import sys
+
+# USAGE:
+#    python test.py <HOST_IP_ADDR> <HOST_PORT>
+#    OR to go into interactive mode, do
+#    python test.py
 
 black_list = ["recv23", "recv21", "recv1F", "recv1C", "recv20", "recv28"]
 
@@ -11,8 +17,13 @@ def t(*args, **kwargs):
     if not pid in black_list:
         print pid, kwargs
 
-HOST = raw_input("HOST: ") or "localhost"
-PORT = int(raw_input("PORT: ") or 25565)
+if len(sys.argv) == 1:  # Interactive mode
+    HOST = raw_input("HOST: ") or "localhost"
+    PORT = int(raw_input("PORT: ") or 25565)
+
+else:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
 
 try:
     with open(".credentials") as f:
